6ceb8b2a7e7321238f706a90af96b6f3667701c2
Weebifying <weebifylmao123@gmail.com>
2024-06-19 19:00:10 +0700

    I FORGOT TO REMOVE LOGS

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@ using namespace geode::prelude;
 class $modify(EditorPauseLayer) {
 	bool init(LevelEditorLayer* editorLayer) {
 		if (!EditorPauseLayer::init(editorLayer)) return false;
-		log::error("EditorPauseLayer::init");
 
 		CCArrayExt<GameObject*> objects = editorLayer->m_objects;
 		int count = 0;
@@ -14,9 +13,7 @@ class $modify(EditorPauseLayer) {
 			if (!obj->m_isHighDetail) count++;
 
 		if (auto menu = this->getChildByID("info-menu")) {
-			log::error("menu");
 			if (auto label = as<CCLabelBMFont*>(menu->getChildByID("object-count-label"))) {
-				log::error("label");
 				auto oldStr = label->getString();
 				label->setString(fmt::format("{} (LDM: {})", oldStr, count).c_str());
 			}
